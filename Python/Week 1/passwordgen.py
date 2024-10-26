aa2a58f0e6ab79d714660d2f5490e51ff9f419bc
keypaa <119448987+keypaa@users.noreply.github.com>
2024-10-26 15:45:46 +0000

    password generator done !

--- Python/Week 1/passwordgen.py
+++ Python/Week 1/passwordgen.py
@@ -3,10 +3,17 @@ characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ&é'(-è_çà)
 # numbers = "1234567890"
 # specials = "&é'(-è_çà)=$^ù*!:;,@^[]#~²"
 # password = ""
-n = input(print("enter a number"))
-random.choice(characters)
+# n = input(print("enter a number"))
+N = input("please enter a number of characters : ")
+N = int(N)
+result = []
+for i in range(N):
+    selected = random.choice(characters)
+    #print(selected)
+    result.append(selected)
 
-
+print(result)
+print("".join(result))
 
 # je veux prendre n nombre de fois au hasard dans ma liste, les ajouter à une liste puis join tout 
 # print("".join(password))
