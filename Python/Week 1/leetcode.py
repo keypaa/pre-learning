1091e1f0d0ed36600eff07045a2bb795a969b5e4
keypaa <119448987+keypaa@users.noreply.github.com>
2024-11-03 15:37:26 +0000

    leetcode

--- Python/Week 1/leetcode.py
+++ Python/Week 1/leetcode.py
@@ -2,15 +2,20 @@ list1 = [1,2,4]
 list2 = [1,3,4]
 
 
-solution = []
+# solution = []
 
-for i in list1:
-    solution.append(i)
-for j in list2:
-    solution.append(j)
+# for i in list1:
+#     solution.append(i)
+# for j in list2:
+#     solution.append(j)
 
-print(solution)
+# print(solution)
 
-solution.sort()
-print(solution)
-
+# solution.sort()
+# print(solution)
+s = "{[]}"
+class Solution:
+    def isValid(self, s: str) -> bool:
+        def has_matchings(s):
+            return "{" in s and "}" in s and "[" in s and "]" in s and "(" in s and ")" in s
+        
